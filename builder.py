791476744c55b0df8439958122d2e7a5acd0164e
vhajsman <vaclav.hajsman@hotmail.com>
2024-01-20 22:45:49 +0100

    build()

--- builder.py
+++ builder.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 
 ext_c =     ["c"]
 ext_cpp =   ["cpp", "cc", "c++"]
@@ -22,25 +23,44 @@ parser.add_argument("-r", "--run", help="Runs executable automatically if suppor
 parser.add_argument("-s", "--script", help="Builds using a script", type=str)
 parser.add_argument("-a", "--arch", help="Specify target architecture (__ARCH_XXX_)", type=str)
 parser.add_argument("-t", "--target", help="Specify target machine (__TARGET_XXX_)", type=str)
-
-parser.add_argument("c", help="Use C", action="store_true")
-parser.add_argument("cpp", help="Use C++", action="store_true")
+parser.add_argument("--timestamp-format", help="Specify timestamp format", type=str)
+parser.add_argument("--timestamp-time", help="Specify timestamp (ignores --timestamp-format)", type=str)
+parser.add_argument("-c", help="Use C", action="store_true", default=True)
+parser.add_argument("-cpp", help="Use C++", action="store_true", default=False)
 
 args = parser.parse_args()
+print(args)
 
 
 def makeQuery():
-    queries = []
+    queries = ["", "", "", ""]
 
-    a = "any" if args.arch == None else parser.arch
-    t = "any" if args.target == None else parser.target
+    a = "any" if args.arch == None else args.arch
+    t = "any" if args.target == None else args.target
 
     queries[0] = "any-any"
     queries[1] = f"{a}-{t}"
     queries[2] = f"{a}-any"
     queries[3] = f"any-{t}"
 
-    return queries;
+    print(queries)
+    return queries
 
 queries = makeQuery()
 
+#def makeTimestamp(format: str):
+    
+
+def callCompiler(isCpp: bool, file: str, arch: str, target: str, optionalDefs: str, optionalParams: str, prefix: str):
+    compiler = {"g++" if isCpp else "gcc"}
+    if prefix != None:
+        compiler = f"{prefix}-{compiler}"
+
+    command = f"{compiler} {file}"
+
+    # Target and architecture
+    command.join(f"-D __ARCH_{arch}_"   if not arch in      ["any", None] else "-D __ARCH_UNKNOWN_ -D __ARCH_x86_")
+    command.join(f"-D __TARGET_{arch}_" if not target in    ["any", None] else "_D __TARGET_UNKNOWN_ -D __ARCH_x86_")
+
+def build():
+    print("Starting build...")
